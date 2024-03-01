14dd693c3ac4f3aa28f31b9c369c10330b803dff
Upasana Devkota <devkotaupasana@gmail.com>
2024-02-29 21:04:57 -0500

    Added schema for user

--- models/HabitList.js
+++ models/HabitList.js
@@ -2,32 +2,43 @@ const mongoose = require("mongoose");
 
 const habitListSchema = new mongoose.Schema(
   {
-    userName: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    scheduledHabitList: [
-      { 
+    user: [
+      {
+        type: new mongoose.Schema(
+          {
+            userName: {
+              type: String,
+              required: true,
+            },
+            email: {
+              type: String,
+              required: true,
+            },
+            password: {
+              type: String,
+              required: true,
+            },
+          },
+          { timestamps: true }
+        ),
+      },
+    ],
+    habit: [
+      {
         type: new mongoose.Schema(
           {
             habitTitle: { type: String },
-            frequency: { type: String }, 
-            completed: { 
+            numValue: { type: Number},
+            unit: { type: String },
+            completed: {
               type: Boolean,
               default: false,
-            }
+            },
           },
-          { timestamps: true }  
-        )
-    }],
+          { timestamps: true }
+        ),
+      },
+    ],
   },
   { timestamps: true }
 );
